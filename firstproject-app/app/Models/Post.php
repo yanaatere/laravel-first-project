6caf3c3123745baa652d501460a882599a27f9c4
yanaatere <yanaandika@gmail.com>
2022-02-06 11:59:08 +0700

    Pertemuan Ke 7: Mass Assignment And Route Model Binding

--- firstproject-app/app/Models/Post.php
+++ firstproject-app/app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /*Ini Yang gak boleh diisi, lainnya gak boleh boleh*/
+    /*protected $fillable = [
+        'title','excerpt','body'
+    ];*/
+
+    /*Ini Yang gak boleh diisi, lainnya boleh*/
+    protected $guarded = ['id'];
 }
