661aa7125b042fe0f0406d15d863e63bde1e8cac
yanatere <lamakau123>
2022-03-21 17:02:21 +0700

    pertemuan Ke 9 : Database Seeder

--- firstproject-app/app/Models/Post.php
+++ firstproject-app/app/Models/Post.php
@@ -21,7 +21,13 @@ class Post extends Model
      * Method Ini berfungsi untuk mengatur relasi ke class category
      * One To Mony
      * */
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- firstproject-app/database/seeders/DatabaseSeeder.php
+++ firstproject-app/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,61 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+//         User::factory(10)->create();
+
+        User::create([
+            'name' => 'Yana Andika',
+            'email' => 'yanaandika@gmail.com',
+            'password' =>bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Dodi',
+            'email' => 'dodi@gmail.com',
+            'password' =>bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title'=> 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title'=> 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title'=> 'Judul ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
